95a4332ce6ce19da3d6259407f176e914984dcda
Mefes <vova_kad@icloud.com>
2015-11-09 15:00:37 +0700

    Добавил изменение настроек Issue #5

---
+++ app/src/main/java/com/example/vladimir/informer_pi/InstStructActivity.java
@@ -0,0 +1,21 @@
+package com.example.vladimir.informer_pi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vladimir Kadochnikov on 06.11.15.
+ */
+public class InstStructActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_institute);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
